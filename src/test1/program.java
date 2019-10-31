45f0ced7947211e91c72c5773aea4758032d6955
hitman91028 <57216680+hitman91028@users.noreply.github.com>
2019-10-31 18:54:43 +0800

    update

---
+++ src/test1/program.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author VicBossHitman
+ */
+//public class program {
+//    
+//}
+//
+
+
+public final class program
+{
+	/** 
+	 The main entry point for the application.
+	*/
+//C# TO JAVA CONVERTER TODO TASK: Java annotations will not correspond to .NET attributes:
+//ORIGINAL LINE: [STAThread] static void Main()
+	public static void main(String[] args)	{
+//		Application.EnableVisualStyles();
+//		Application.SetCompatibleTextRenderingDefault(false);
+//		Application.Run(new Form1());
+            System.out.println("Hello, world");
+	}
+}
